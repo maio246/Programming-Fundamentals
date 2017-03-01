a2f8915d00e2a68d90f6d0f521caaabfebf80518
maio246 <maio246@gmail.com>
2017-03-01 21:54:29 +0200

    Homework progress

---
+++ Lists excercises/fold and sum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fold_and_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
